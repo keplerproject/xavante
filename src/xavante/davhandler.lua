212ca5d00c098ef52f818b1860df6260eaf4514a
jguerra <jguerra>
2005-05-07 04:00:54 +0000

    - PUT handler - refactored method dispatcher - handles XML default namespaces - request headers are lowercased - handles <propname> and <allprop> queries in PROPFIND - addProp() and addPropStat() handle <propstat> tags, joining and sorting responses by response status - uses relative path inside source and properties objects - lomContent() returns XML content of a node - added lomChildsByTagName() (like a shallow lomElementsByTagName()) - reducename returns unmodified name when there's no namespace to extract - PROPPATCH handler

--- src/xavante/davhandler.lua
+++ src/xavante/davhandler.lua
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------------
 -- Xavante webDAV handler
 -- Author: Javier Guerra
--- Copyright (c) 2004-2005 Javier Guerra
+-- Copyright (c) 2005 Javier Guerra
 -----------------------------------------------------------------------------
 require "lxp.lom"
 require "socket.url"
@@ -55,14 +55,13 @@ end
 -- gets request content, and parses it as XML
 -- returns LOM object
 local function req_xml (req)
-	local sz = req.headers ["Content-Length"]
+	local sz = req.headers ["content-length"]
 	local indata
 	if sz then
 		indata = req.socket:receive (sz)
 	else
 		indata = function () return req.socket:receive () end
 	end
-	
 	return lxp.lom.parse (indata)
 end
 
@@ -76,6 +75,11 @@ local function xml_ns (x, dict)
 	
 	-- adds any new namespace to the dictionary
 	for i,attr_name in ipairs (x.attr) do
+		-- default namespace
+		if attr_name == "xmlns" then
+			dict [""] = x.attr [attr_name]
+		end
+		-- named namespaces
 		local _,_, ns_var = string.find (attr_name, "xmlns:(.*)$")
 		if ns_var then
 			dict [ns_var] = x.attr [attr_name]
@@ -88,6 +92,9 @@ local function xml_ns (x, dict)
 		local pat = string.format ("^%s:", ns)
 		x.tag = string.gsub (x.tag, pat, dict[ns])
 	end
+	if not ns and dict [""] then
+		x.tag = dict [""] .. x.tag
+	end
 	
 	-- recurses to child nodes
 	for _, sub in ipairs (x) do
@@ -95,6 +102,22 @@ local function xml_ns (x, dict)
 	end
 
 	return x
+end
+
+-- iterator for traversing only childs in a LOM object
+-- whith a given tagname.
+local function lomChildsByTagName (x, tagname)
+	local function gen (x)
+		for _,elem in ipairs (x) do
+			if type(elem) == "table" then
+				if elem.tag and elem.tag == tagname then
+					coroutine.yield (elem)
+				end
+			end
+		end
+	end
+
+	return coroutine.wrap (function () gen (x) end)
 end
 
 -- iterator for traversing all elements in a LOM object
@@ -128,6 +151,18 @@ local function lomChilds (x)
 	return coroutine.wrap (gen)
 end
 
+-- returns all the text content of a LOM node
+local function lomContent (x)
+	local out = {}
+	local function of (s)
+		table.insert (out, s)
+	end
+	for _, child in ipairs (x) do
+		lomtoxml (child, of)
+	end
+	return table.concat (out)
+end
+
 -- returns a table member of a table, creates it if needed
 -- params:
 --		t: table
@@ -145,7 +180,7 @@ end
 --		name:	tagname to reduce
 -- returns new tagname, ns is modified in-place if needed
 local function reducename (ns, name)
-	if string.find (name, "[:/]") then
+	if string.find (name, "[:/?#]") then
 		local _,_,pfx,sfx = string.find (name, "(.*%W)(%w+)")
 		local n = 0
 		for k,v in pairs (ns) do
@@ -157,6 +192,8 @@ local function reducename (ns, name)
 		local newns = "lm"..n
 		ns [newns] = pfx
 		return string.format ("%s:%s", newns, sfx)
+	else
+		return name
 	end
 end
 
@@ -169,18 +206,58 @@ local function nsattr (ns)
 	return table.concat (attr, " ")
 end
 
-local function dav_propfind (req, res, repos)
+local function addProp (outtable, propname, propval)
+	if propval then
+		local propentry = maketabletable (outtable, "HTTP/1.1 200 OK")
+		if propval == "" then
+			table.insert (propentry, string.format ([[<%s/>]], propname))
+		else
+			table.insert (propentry, 
+				string.format ([[<%s>%s</%s>]], 
+					propname, propval, propname))
+		end
+	else
+		local propentry = maketabletable (outtable, "HTTP/1.1 404 NotFound")
+		table.insert (propentry, string.format ([[<%s/>]], propname))
+	end
+end
+
+local function addPropstat (outtable, propstat)
+	local codes = {}
+	for stat in pairs (propstat) do
+		table.insert (codes, stat)
+	end
+	table.sort (codes)
+	for _, stat in ipairs (codes) do
+		props = propstat [stat]
+		if props then
+			table.insert (outtable, [[<D:propstat>]])
+			table.insert (outtable, [[<D:prop>]])
+			for _,prop in ipairs (props) do
+				table.insert (outtable, prop)
+			end
+			table.insert (outtable, [[</D:prop>]])
+			table.insert (outtable, string.format ([[<D:status>%s</D:status>]], stat))
+			table.insert (outtable, [[</D:propstat>]])
+		end
+	end
+end
+
+local function dav_propfind (req, res, repos_b, props_b)
 	res.statusline = "HTTP/1.1 207 Multi-Status\r\n"
 	res.headers ["Content-Type"] = 'text/xml; charset="utf-8"'
 
-	local depth = req.headers.Depth
-	local path = url.unescape (req.parsed_url.path)
+	local depth = req.headers.depth
+	local path = req.relpath
 	local data = xml_ns (req_xml (req))
 
---	print ("path:", path)
+--	print ("path:", path, req.relpath)
 --	print ("depth:", depth)
 
-	local resource_q = repos:getResource (path)
+--	print ("como xml:") lomtoxml (data) print ()
+
+
+	local resource_q = repos_b:getResource (req.match, path)
 	if not resource_q then
 		return httpd.err_404 (req, res)
 	end
@@ -189,45 +266,59 @@ local function dav_propfind (req, res, repos)
 
 	table.insert (content, [[<?xml version="1.0" encoding="utf-8" ?>]])
 	table.insert (content, [[<D:multistatus xmlns:D="DAV:">]])
+	local namespace = {}
 
-	local propval
+	assert (data.tag == "DAV:propfind")
 	for resource in resource_q:getItems (depth) do
 	--	print ("resource:", resource.path)
 		local propstat = {}
-		local namespace = {D="DAV:"}
-		for propgroup in lomElementsByTagName (data, "DAV:prop") do
-			for _,propname in lomChilds (propgroup) do
-				local propval = resource:getProp (propname)
-				local shortname = reducename (namespace, propname)
-				if propval then
-					local propentry = maketabletable (propstat, "HTTP/1.1 200 OK")
-					if propval == "" then
-						table.insert (propentry, string.format ([[<%s />]], shortname))
-					else
-						table.insert (propentry, 
-							string.format ([[<%s>%s</%s>]], 
-								shortname, propval, shortname))
+		namespace.D="DAV:"
+
+		for findtype, findtype_tn in lomChilds (data) do
+
+			if findtype_tn == "DAV:prop" then
+				for _, propname in lomChilds (findtype) do
+					local propval = resource:getProp (propname)
+					if not propval and props_b then
+						propval = props_b:getProp (req.relpath, propname)
 					end
-				else
-					local propentry = maketabletable (propstat, "HTTP/1.1 404 NotFound")
-					table.insert (propentry, string.format ([[<%s />]], shortname))
+					local shortname = reducename (namespace, propname)
+					addProp (propstat, shortname, propval)
+				end
+
+			elseif findtype_tn == "DAV:propname" then
+				for propname in resource:getPropNames () do
+					local shortname = reducename (namespace, propname)
+					addProp (propstat, shortname, "")
+				end
+				if props_b then
+					for propname in props_b:getPropNames (req.relpath) do
+						local shortname = reducename (namespace, propname)
+						addProp (propstat, shortname, "")
+					end
+				end
+
+			elseif findtype_tn == "DAV:allprop" then
+				for propname in resource:getPropNames () do
+					local shortname = reducename (namespace, propname)
+					local propval =  resource:getProp (propname)
+					addProp (propstat, shortname, propval)
+				end
+				if props_b then
+					for propname in props_b:getPropNames (req.relpath) do
+						local shortname = reducename (namespace, propname)
+						local propval = props_b:getProp (req.relpath, propname)
+						addProp (propstat, shortname, propval)
+					end
 				end
 			end
 		end
+	
+		namespace.D = nil
 
 		table.insert (content, string.format ([[<D:response %s>]], nsattr (namespace)))
 		table.insert (content, string.format ([[<D:href>%s</D:href>]], resource:getHRef()))
-		for stat,props in pairs (propstat) do
-			table.insert (content, [[<D:propstat>]])
-			table.insert (content, [[<D:prop>]])
-			for _,prop in ipairs (props) do
-				table.insert (content, prop)
-			end
-			table.insert (content, [[</D:prop>]])
-			table.insert (content, string.format ([[<D:status>%s</D:status>]], stat))
-			table.insert (content, [[</D:propstat>]])
-		end
-
+		addPropstat (content, propstat)
 		table.insert (content, [[</D:response>]])
 	end
 	
@@ -237,23 +328,67 @@ local function dav_propfind (req, res, repos)
 
 	res.content = content
 	return res
-
 end
 
-local function dav_proppatch (req, res, repos)
+local function dav_proppatch (req, res, repos_b, props_b)
+	res.statusline = "HTTP/1.1 207 Multi-Status\r\n"
+	res.headers ["Content-Type"] = 'text/xml; charset="utf-8"'
+
+	local path = req.relpath
 	local data = xml_ns (req_xml (req))
 --	print ("como xml:") lomtoxml (data) print ()
+
+	local resource = repos_b:getResource (req.match, path)
+	if not resource then
+		return httpd.err_404 (req, res)
+	end
+
+	local content = {}
+
+	table.insert (content, [[<?xml version="1.0" encoding="utf-8" ?>]])
+	local namespace = {D="DAV:"}
+	local propstat = {}
+
+	assert (data.tag == "DAV:propertyupdate")
+	for cmd_node, cmd_name in lomChilds (data) do
+		for nd in lomChildsByTagName (cmd_node, "DAV:prop") do
+			local status = nil
+			for prop_node, propname in lomChilds (nd) do
+				if cmd_name == "DAV:set" then
+					props_b:setProp (path, propname, lomContent (prop_node))
+					status = "HTTP/1.1 200 OK"
+				elseif cmd_name == "DAV:remove" then
+					props_b:setProp (path, propname, nil)
+					status = "HTTP/1.1 200 OK"
+				end
+
+				if status then
+					local propentry = maketabletable (content, status)
+					table.insert (propentry, string.format ([[<%s/>]], reducename (namespace, propname)))
+				end
+			end
+		end
+	end
+
+	table.insert (content, string.format ([[<D:multistatus %s>]], nsattr (namespace)))
+	table.insert (content, [[<D:response>]])
+	table.insert (content, string.format ([[<D:href>%s</D:href>]], resource:getHRef()))
+	addPropstat (content, propstat)
+	table.insert (content, [[</D:response>]])
+	table.insert (content, [[</D:multistatus>]])
+
+	res.content = content
 	return res
 end
 
-local function dav_options (req, res, repos)
+local function dav_options (req, res, repos_b, props_b)
 	res.headers ["DAV"] = "1,2"
 	res.content = ""
 	return res
 end
 
-local function dav_get (req, res, repos)
-	local resource = repos:getResource (url.unescape (req.parsed_url.path))
+local function dav_get (req, res, repos_b, props_b)
+	local resource = repos_b:getResource (req.match, req.relpath)
 	if not resource then
 		return httpd.err_404 (req, res)
 	end
@@ -262,28 +397,55 @@ local function dav_get (req, res, repos)
 	res.headers ["Content-Length"] = resource:getContentSize () or 0
 
 	httpd.send_res_headers (res)
-	for block in resource:getResourceData () do
+	for block in resource:getContentData () do
 		httpd.send_res_data (res, block)
 	end
 	return res
 end
 
+local function dav_put (req, res, repos_b)
+	local path = req.relpath
+	local resource = repos_b:getResource (req.match, path)
+		or repos_b:createResource (req.match, path)
 
-function makeHandler (repos)
-	local dav_cmd_dispatch = {
-		PROPFIND = function (req, res) return dav_propfind (req, res, repos) end,
-		PROPPATCH = function (req, res) return dav_proppatch (req, res, repos) end,
-		OPTIONS = function (req, res) return dav_options (req, res, repos) end,
-		GET = function (req, res) return dav_get (req, res, repos) end,
-	}
+	if req.headers["content-range"] then
+		return httpd.err_405 (req, res)
+	end
 
+	local contentlength = assert (req.headers ["content-length"]) + 0
+	local bsz
+	while contentlength > 0 do
+		if contentlength > 4096 then bsz = 4096 else bsz = contentlength end
+		resource:addContentData (req.socket:receive (bsz))
+		contentlength = contentlength - bsz
+	end
+	httpd.send_res_headers (res)
+	return res
+end
+
+local function dav_lock (req, res, repos_b)
+	local data = xml_ns (req_xml (req))
+	print ("como xml:") lomtoxml (data) print ()
+	return res
+end
+
+local dav_cmd_dispatch = {
+	PROPFIND = dav_propfind,
+	PROPPATCH = dav_proppatch,
+	OPTIONS = dav_options,
+	GET = dav_get,
+	PUT = dav_put,
+	LOCK = dav_lock,
+}
+
+function makeHandler (repos_b, props_b)
 	return function (req, res)
-	--	print (req.cmd_mth, req.parsed_url.path)
-	--	for k,v in pairs (req.headers) do print (k,v) end
+--		print (req.cmd_mth, req.parsed_url.path)
+--		for k,v in pairs (req.headers) do print (k,v) end
 
 		local dav_handler = dav_cmd_dispatch [req.cmd_mth]
 		if dav_handler then
-			return dav_handler (req, res)
+			return dav_handler (req, res, repos_b, props_b)
 		end
 	end
 end
