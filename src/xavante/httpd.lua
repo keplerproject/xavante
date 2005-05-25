3b085866fb5df9219a0acb05b5986ea7b684d44e
jguerra <jguerra>
2005-05-25 21:38:50 +0000

    send_res_headers(res) and send_res_data(res,data) are local; handlers should now use OO forms res:send_headers() and res:send_data(data)

--- src/xavante/httpd.lua
+++ src/xavante/httpd.lua
@@ -185,17 +185,9 @@ function handle_request (req, res)
 	return h (req, res)
 end
 
-function make_response (req)
-	local res = {
-		socket = req.socket,
-		headers = default_headers (req),
-	}
-	return res
-end
-
 -- sets the default response headers
 function default_headers (req)
-	return  {	
+	return  {
 		Date = os.date ("!%a, %d %b %Y %H:%M:%S GMT"),
 		Server = _serversoftware,
 	}
@@ -208,7 +200,7 @@ end
 --		res.sent_headers : if true, headers are already sent, does nothing
 --		res.statusline : response status, if nil, sends 200 OK
 --		res.headers : table of header fields to send
-function send_res_headers (res)
+local function send_res_headers (res)
 	if (res.sent_headers) then
 		return
 	end
@@ -229,7 +221,7 @@ end
 -- params:
 --		res ; response object
 --		data : content data to send
-function send_res_data (res, data)
+local function send_res_data (res, data)
 
 	if not data then
 		return
@@ -242,6 +234,17 @@ function send_res_data (res, data)
 	if data then
 		res.socket:send (data)
 	end
+end
+
+function make_response (req)
+	local res = {
+		req = req,
+		socket = req.socket,
+		headers = default_headers (req),
+		send_headers = send_res_headers,
+		send_data = send_res_data,
+	}
+	return res
 end
 
 -- sends prebuilt content to the client
