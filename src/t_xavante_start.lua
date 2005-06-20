43a5bf6cb585058edbd132af4e4867f49045950f
carregal <carregal>
2005-06-20 22:49:37 +0000

    Documentation update, version 1.1 New directory structure ($LUA, $BIN, $CONF, $WEB)

--- src/t_xavante_start.lua
+++ src/t_xavante_start.lua
@@ -2,21 +2,17 @@
 -------------------------------------------------------------------------------
 -- Starts the Xavante Web server.
 --
--- See /bin/xavante/config.lua for configuration details.
+-- See xavante/config.lua for configuration details.
 --
 -- Authors: Javier Guerra and Andre Carregal
 -- Copyright (c) 2004-2005 Kepler Project
 -------------------------------------------------------------------------------
 
--- Physical location of Xavante file structure. The default structure assumes
--- that the bin, conf and web directories are under the same directory
-local XAVANTE_HOME = [[XAVANTE_HOME]]
-local XAVANTE_BIN  = [[XAVANTE_BIN]]  -- used by require
-local XAVANTE_CONF = [[XAVANTE_CONF]] -- configuration files
-local XAVANTE_WEB  = [[XAVANTE_WEB]]  -- documents and scripts
+-- Default directory for documents and scripts
+local XAVANTE_WEB  = [[XAVANTE_WEB]]  
 
--- compatibility code for Lua version 5.0 providing 5.1 behavior
-if string.find (_VERSION, "Lua 5.0") and not package then
+--- compatibility code for Lua version 5.0 providing 5.1 behavior
+if string.find (_VERSION, "Lua 5.0") and not _COMPAT51 then
 	if not LUA_PATH then
 		LUA_PATH = [[LUA_PATH]]
 	end
@@ -32,6 +28,6 @@ xavante.setwebdir(XAVANTE_WEB)
 -- Loads the configuration file and starts Xavante
 --
 -- XAVANTE_ISFINISHED and XAVANTE_TIMEOUT are optional globals that can
--- control how xavante will behave when externally controlled.
+-- control how Xavante will behave when externally controlled.
 -------------------------------------------------------------------------------
 xavante.start(XAVANTE_ISFINISHED, XAVANTE_TIMEOUT)
