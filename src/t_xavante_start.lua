d809998e5c44096a5713117f5198873606648be2
carregal <carregal>
2005-06-21 18:59:09 +0000

    Documentation update

--- src/t_xavante_start.lua
+++ src/t_xavante_start.lua
@@ -8,16 +8,23 @@
 -- Copyright (c) 2004-2005 Kepler Project
 -------------------------------------------------------------------------------
 
--- Default directory for documents and scripts
-local XAVANTE_WEB  = [[XAVANTE_WEB]]  
+local LIB_EXT      = [[LIB_EXT]]
+local XAVANTE_HOME = [[XAVANTE_HOME]]
+local XAVANTE_BIN  = XAVANTE_HOME.."/bin/"  
+local XAVANTE_CONF = XAVANTE_HOME.."/conf/"
+local XAVANTE_LUA  = XAVANTE_HOME.."/lua/"
+local XAVANTE_WEB  = XAVANTE_HOME.."/web/"
 
 --- compatibility code for Lua version 5.0 providing 5.1 behavior
 if string.find (_VERSION, "Lua 5.0") and not _COMPAT51 then
 	if not LUA_PATH then
-		LUA_PATH = [[LUA_PATH]]
+		LUA_PATH = XAVANTE_CONF.."?.lua;"..
+                   XAVANTE_LUA.."?.lua;"..
+                   XAVANTE_LUA.."?/?.lua;"..
+                   XAVANTE_LUA.."?/init.lua;"
 	end
 	require"compat-5.1"
-	package.cpath = [[LUA_CPATH]]
+	package.cpath = XAVANTE_BIN.."?."..LIB_EXT
 end
 
 require "xavante.server"
