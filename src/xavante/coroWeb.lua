72f1bacec057fb0a7bf15fa5e05b1b41af7f6853
uid20103 <uid20103>
2005-08-16 18:38:28 +0000

    - "refresh" return flag in event handlers - error reporting

--- src/xavante/coroWeb.lua
+++ src/xavante/coroWeb.lua
@@ -9,13 +9,16 @@ local session = require "xavante.session"
 
 module (arg and arg[1])
 
+local function _error (res, err)
+	res:send_data ("<h1>error:"..err.."</h1>")
+end
+
 function coroHandler (name, h)
 	return function (req, res)
 		local sess = session.open (req, res, name)
 		sess.coHandler = sess.coHandler or coroutine.create (h)
 		
 		local ok, err = coroutine.resume (sess.coHandler, req, res)
-		
 		
 		if not ok then _error (res, err) end
 		
@@ -27,15 +30,20 @@ function coroHandler (name, h)
 	end
 end
 
-function event (sh_t, get_all)
+function event (in_req, sh_t, get_all)
 	local req, res
-	local subH
+	local subH, ret
 	repeat
 		req, res = coroutine.yield ()
 		subH = sh_t [req.relpath]
 		if subH and type (subH) == "function" then
-			subH (req, res)
+			ret = subH (req, res)
 		end
-	until not subH or get_all
+		if ret == "refresh" then
+--			xavante.httpd.redirect (res, string.gsub (req.parsed_url.path, "/[^/]*$", ""))
+			xavante.httpd.redirect (res, in_req.parsed_url.path)
+			req, res = coroutine.yield ()
+		end
+	until not subH or get_all or ret == "refresh" 
 	return req, res
 end
