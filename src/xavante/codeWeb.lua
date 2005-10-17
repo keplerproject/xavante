f61a6a0fd5898cb277c6ee95581b95c4df6755e4
jguerra <jguerra>
2005-10-17 01:22:42 +0000

    support for setting the environment of template-based handlers

--- src/xavante/codeWeb.lua
+++ src/xavante/codeWeb.lua
@@ -8,6 +8,11 @@
 require "cgilua.prep"
 module (arg and arg[1])
 
+-- Adds a module as a website;
+-- each public function is registered as a page handler
+--	host,urlpath : where to insert the handlers
+--	m : table with functions, a module
+--	register_as_tree: if true, each handler is registered as a directory too
 function addModule (host, urlpath, m, register_as_tree)
 	if m.__main then
 		xavante.httpd.addHandler (host, urlpath, m.__main)
@@ -27,7 +32,23 @@ function addModule (host, urlpath, m, register_as_tree)
 	end
 end
 
-function load_cw (fname)
+-- Builds a handler from a template
+-- the template (loaded from file 'fname') uses LuaPage syntax
+-- and is converted to a function in the form: "function (req,res,...)"
+-- if env is a table, the resulting function uses it as environment
+-- if it's a function, it's environment is used
+-- if it's a number, it select's the environment from the call stack
+-- if ommited, 1 is implied, (the calling function's environment)
+function load_cw (fname, env)
+
+	env = env or 1
+
+	if type (env) == "function" then
+		env = getfenv (env)
+	elseif type (env) == "number" then
+		env = getfenv (env+1)
+	end
+
 	local fh = assert (io.open (fname))
 	local prog = fh:read("*a")
 	fh:close()
@@ -37,7 +58,9 @@ function load_cw (fname)
 	if prog then
 		local f, err = loadstring (prog, "@"..fname)
 		if f then
-			return f()
+			local f_cw = f()
+			if env then setfenv (f_cw, env) end
+			return f_cw
 		else
 			error (err)
 		end
