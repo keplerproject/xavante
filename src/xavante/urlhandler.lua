2addf337e31fc3bd28c9572c301140641d52304d
jguerra <jguerra>
2006-09-28 17:00:10 +0000

    *** empty log message ***

--- src/xavante/urlhandler.lua
+++ src/xavante/urlhandler.lua
@@ -1,3 +1,11 @@
+-----------------------------------------------------------------------------
+-- Xavante URL paths handler
+--
+-- Authors: Javier Guerra
+-- Copyright (c) 2006 Kepler Project
+--
+-- $Id: urlhandler.lua,v 1.2 2006/09/28 17:00:10 jguerra Exp $
+-----------------------------------------------------------------------------
 
 local function path_p (s, p)
 	if not p then return s end
--- src/xavante/vhostshandler.lua
+++ src/xavante/vhostshandler.lua
@@ -1,3 +1,11 @@
+-----------------------------------------------------------------------------
+-- Xavante virtual hosts handler
+--
+-- Authors: Javier Guerra
+-- Copyright (c) 2006 Kepler Project
+--
+-- $Id: vhostshandler.lua,v 1.2 2006/09/28 17:00:10 jguerra Exp $
+-----------------------------------------------------------------------------
 
 function xavante.vhostshandler (vhosts)
 	return function (req, res)
