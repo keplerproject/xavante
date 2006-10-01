7263fa93e744e977b5f9b9b6587f9f38cb4801ab
jguerra <jguerra>
2006-10-01 18:38:48 +0000

    - some refactoring and commenting - handles partial requests (only single ranges)

--- src/xavante/filehandler.lua
+++ src/xavante/filehandler.lua
@@ -4,13 +4,70 @@
 -- Authors: Javier Guerra and Andre Carregal
 -- Copyright (c) 2004-2006 Kepler Project
 --
--- $Id: filehandler.lua,v 1.13 2006/09/28 16:49:36 jguerra Exp $
+-- $Id: filehandler.lua,v 1.14 2006/10/01 18:38:48 jguerra Exp $
 ----------------------------------------------------------------------------
 
 local lfs = require "lfs"
 local url = require "socket.url"
 require "xavante.mime"
 
+-- gets the mimetype from the filename's extension
+local function mimefrompath (path)
+	local _,_,exten = string.find (path, "%.([^.]*)$")
+	if exten then
+		return xavante.mimetypes [exten]
+	else
+		return nil
+	end
+end
+
+-- on partial requests seeks the file to
+-- the start of the requested range and returns
+-- the number of bytes requested.
+-- on full requests returns nil
+local function getrange (req, f)
+	local range = req.headers["range"]
+	if not range then return nil end
+	
+	local s,e, r_A, r_B = string.find (range, "(%d*)%s*-%s*(%d*)")
+	if s and e then
+		r_A = tonumber (r_A)
+		r_B = tonumber (r_B)
+		
+		if r_A then
+			f:seek ("set", r_A)
+			if r_B then return r_B + 1 - r_A end
+		else
+			if r_B then f:seek ("end", r_B) end
+		end
+	end
+	
+	return nil
+end
+
+-- sends data from the open file f
+-- to the response object res
+-- sends only numbytes, or until the end of f
+-- if numbytes is nil
+local function sendfile (f, res, numbytes)
+	local block
+	local whole = not numbytes
+	local left = numbytes
+	local blocksize = 8192
+	
+	while whole or left > 0 do
+		block = f:read (blocksize)
+		if not block then return end
+		if not whole then
+			left = left - string.len (block)
+			blocksize = math.min (blocksize, left)
+		end
+		res:send_data (block)
+	end
+end
+
+
+-- main handler
 local function filehandler (req, res, baseDir)
 
 	if req.cmd_mth ~= "GET" and req.cmd_mth ~= "HEAD" then
@@ -19,12 +76,7 @@ local function filehandler (req, res, baseDir)
 
 	local path = baseDir .."/".. req.relpath
 	
-	local _,_,exten = string.find (path, "%.([^.]*)$")
-	exten = exten or ""
-	local mimetype = xavante.mimetypes [exten]
-	if mimetype then
-		res.headers ["Content-Type"] = mimetype
-	end
+	res.headers ["Content-Type"] = mimefrompath (path)
 	
 	local attr = lfs.attributes (path)
 	if not attr then
@@ -47,13 +99,13 @@ local function filehandler (req, res, baseDir)
 	end
 		
 	if req.cmd_mth == "GET" then
-		local block
-		repeat
-			block = f:read (8192)
-			if block then
-				res:send_data (block)
-			end
-		until not block
+		local range_len = getrange (req, f)
+		if range_len then
+			res.statusline = "HTTP/1.1 206 Partial Content\r\n"
+			res.headers["Content-Length"] = range_len
+		end
+		
+		sendfile (f, res, range_len)
 		f:close ()
 	end
 	
